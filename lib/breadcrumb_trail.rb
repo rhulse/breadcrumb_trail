834f1313a2a90d99402c28832f50490f1fcd64ae
Richard Hulse <richard.hulse@radionz.co.nz>
2009-06-01 14:36:10 +1200

    added a bunch of new features and added to docs
    1. remove link from last crumb automatically
    
    This allows you to add crumbs anywhere and never have to worry
    about which one is last (and which to omit the link on)
    
    If you forget a link it get printed anyway.
    
    2. Different styles
    
    The crumbs can be printed as an
    
     <ol> (default)
     <ul>
     <p> (with a separator)
    
    the class for the list and the separator for the <p>
    can be overridden
    
    3. Docs improved to show simpler way to add the crumbs
    
    A before_filter in the controller adds the controller crumb
    for every action making the action code DRYer.

--- lib/breadcrumb_trail.rb
+++ lib/breadcrumb_trail.rb
@@ -1,10 +1,12 @@
 module BreadcrumbTrail
-  def add_root_crumb(crumb={})
-    defaults = {:link => root_path, :label => 'Home', :class => 'first'}
-    @crumbs = [defaults.merge(crumb)]
-  end
   def add_crumb(crumb)
     add_root_crumb unless @crumbs
     @crumbs << crumb
+  end
+
+  private
+  def add_root_crumb(crumb={})
+    defaults = {:link => root_path, :label => 'Home', :class => 'first'}
+    @crumbs = [defaults.merge(crumb)]
   end
 end
--- lib/breadcrumb_trail_helper.rb
+++ lib/breadcrumb_trail_helper.rb
@@ -1,14 +1,30 @@
 module BreadcrumbTrailHelper
-  def crumbs
-    if @crumbs
-      list_items = String.new
-      
-      @crumbs.each do |c|
-        list_items << content_tag(:li, link_to(h(c[:label]), c[:link]), :class => c[:class]) if c[:link]
-        list_items << content_tag(:li, h(c[:label]), :class => c[:class]) unless c[:link]
-      end
-      
-      content_tag :ol, list_items, :class => 'breadcrumb'
+  def render_crumbs(options={})
+	  return nil if @crumbs.nil?
+
+    defaults = {:type => :ol, :class => 'breadcrumbs', :separator => ' &raquo; '}
+    options = defaults.merge(options)
+
+    list_items = Array.new
+
+    case options[:type]
+      when :ol, :ul
+        @crumbs[0..-2].each do |c|
+          list_items << content_tag(:li, link_to(h(c[:label]), c[:link]), :class => c[:class]) if c[:link]
+          list_items << content_tag(:li, h(c[:label]), :class => c[:class]) unless c[:link]
+        end
+        c = @crumbs.last
+        list_items << content_tag(:li, h(c[:label]), :class => c[:class])
+        content_tag options[:type], list_items, :class => options[:class]
+      when :p
+        @crumbs[0..-2].each do |c|
+          list_items << link_to(h(c[:label]), c[:link], :class => c[:class]) if c[:link]
+          list_items << h(c[:label]) unless c[:link]
+        end
+        c = @crumbs.last
+        list_items << h(c[:label]) unless c[:link]
+        content_tag :p, list_items.join(options[:separator]), :class => options[:class]
+      else
     end
   end
 end
